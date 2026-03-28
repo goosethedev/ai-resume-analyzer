1791713393b5d322a5e7a484259f6d0cdc0a7700
Gus Sánchez <goosethedev@proton.me>
2026-03-28 12:33:17 -0400

    head info managed by tanstack

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,11 +6,31 @@ import {
 } from "@tanstack/solid-router";
 import { TanStackRouterDevtools } from "@tanstack/solid-router-devtools";
 
-import styleCss from "../styles.css?url";
-
 export const Route = createRootRouteWithContext()({
   head: () => ({
-    links: [{ rel: "stylesheet", href: styleCss }],
+    meta: [
+      { title: "Resumind" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        name: "description",
+        content: "AI Resume Analyzer",
+      },
+      {
+        name: "keywords",
+        content: "resume, ai, resume analyzer, resume analyzer",
+      },
+      {
+        name: "charset",
+        content: "utf-8",
+      },
+    ],
+    links: [
+      {
+        rel: "icon",
+        type: "image/svg+xml",
+        href: "/favicon.ico",
+      },
+    ],
   }),
   component: RootComponent,
   notFoundComponent: () => <h1>Page not found!</h1>,
