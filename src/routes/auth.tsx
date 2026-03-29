df4178758c4489397c02909a47dc92dbad7fcc8a
Gus Sánchez <goosethedev@proton.me>
2026-03-29 11:45:02 -0400

    auth: layout finished

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from "@tanstack/solid-router";
+import { puter } from "@heyputer/puter.js";
+import { createSignal, Show } from "solid-js";
 
 export const Route = createFileRoute("/auth")({
   component: RouteComponent,
@@ -11,6 +13,7 @@ export const Route = createFileRoute("/auth")({
 });
 
 function RouteComponent() {
+  const [isLoading, setLoading] = createSignal(false);
   return (
     <main class="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
       <div class="gradient-border shadow-lg">
@@ -18,6 +21,18 @@ function RouteComponent() {
           <div class="flex flex-col items-center gap-2 text-center">
             <h1>Welcome</h1>
             <h2>Log In to Continue Your Job Journey</h2>
+          </div>
+          <div>
+            <Show when={!isLoading()} fallback={<button class="auth-button animate-pulse">
+              <p>Signing you in...</p>
+              </button>}>
+              <Show when={puter.auth.isSignedIn()}>
+                <button class="auth-button" onClick={puter.auth.signOut}><p>Log Out</p></button>
+              </Show>
+              <Show when={!puter.auth.isSignedIn()}>
+                <button class="auth-button" onClick={puter.auth.signIn}><p>Log In</p></button>
+              </Show>
+            </Show>
           </div>
         </section>
       </div>
